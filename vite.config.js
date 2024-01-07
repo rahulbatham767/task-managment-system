84033ec1fc044b29c3417ed73870782848fb4346
rahulbatham767 <rahulbatham767@gmail.com>
2024-01-07 06:12:28 +0530

    build added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react-redux"], // Add this line
+    },
+  },
   esbuild: {
-    // jsxInject: `import React from 'react';`, // Add this line
+    // jsxInject: `import React from 'react';`, // Add this line if needed
   },
 });
