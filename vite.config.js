275303e3a95833ce48e72259b4bf1feb513cdd48
rahulbatham767 <rahulbatham767@gmail.com>
2024-01-08 22:08:48 +0530

    redux problem fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./",
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ["@reduxjs/toolkit", "react-redux"], // Add this line
-    },
+    // rollupOptions: {
+    //   external: ["@reduxjs/toolkit", "react-redux"], // Add this line
+    // },
   },
   esbuild: {
     // jsxInject: `import React from 'react';`, // Add this line if needed
@@ -17,12 +16,5 @@ export default defineConfig({
     include: ["@reduxjs/toolkit"],
   },
   base: "/task-managment-system/",
-  server: {
-    fs: {
-      allow: [
-        "L:/Project/Task Managment System/node_modules/@fortawesome/fontawesome-free/webfonts/", // Add this line
-        "L:/Project/Task Managment System/Task Managment System/src/", // Add this line
-      ],
-    },
-  },
+  ssr: { noExternal: ["@reduxjs/toolkit"] },
 });
